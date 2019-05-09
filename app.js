f44853a256462f92ab93d86e4a0a41f119d10f4c
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-09 10:45:21 +0545

    add validation to the create-post

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("Connected..."))
   .catch(err => {
-    console.log("DB connection error");
+    console.log("DB connection error", err);
   });
 
 // middleware
 app.use(morgan("dev"));
-app.use("/", require("./routes/post.js"));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use("/", require("./routes/post"));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,18 @@
+const Post = require("../models/Post");
+
 exports.getPosts = (req, res) => {
- res.json({
-     posts: [{ title: "First post" }, { title: "Second post" }]
- });
+  res.json({
+    posts: [{ title: "First post" }, { title: "Second post" }]
+  });
+};
+
+exports.createPost = async (req, res) => {
+  const post = new Post(req.body);
+
+  const savedpost = await post.save();
+
+  res.status(200).json({
+    post: savedpost
+  });
+
 };
