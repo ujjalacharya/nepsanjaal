a0d0f3e810106da3b9339000d240af25d79ed006
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-11 09:06:34 +0545

    add signin method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
   const userExists = await User.findOne({ email: req.body.email });
@@ -9,4 +10,32 @@ exports.signup = async (req, res) => {
   const user = await new User(req.body);
   await user.save();
   res.status(200).json({ message: "Signup success! Please login." });
+};
+
+exports.signin = async (req, res) => {
+  // find the user based on email
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  // if err or no user
+  if (!user) {
+    return res.status(401).json({
+      error: "User with that email does not exist."
+    });
+  }
+  // if user is found make sure the email and password match
+  // create authenticate method in model and use here
+  if (!user.authenticate(password)) {
+    return res.status(401).json({
+      error: "Email and password do not match"
+    });
+  }
+  // generate a token with user id and secret
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  // persist the token as 't' in cookie with expiry date
+  res.cookie("t", token, { expire: new Date() + 9999 });
+  // retrun response with user and token to frontend client
+
+  const {_id, email: mail, name} = user;
+  return res.json({ token, user: {_id, mail, name}});
 };
