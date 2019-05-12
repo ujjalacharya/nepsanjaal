9f67e61a2cc3e1bb819428dd6b0e8d603b8f8029
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-12 14:19:00 +0545

    add signup page

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./core/Home";
+import Signup from "./user/Signup";
 
 const MainRouter = () => {
   return (
     <Switch>
-      <Route to="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={Signup} />
     </Switch>
   );
 };
---
+++ client/src/user/Signup.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+
+class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      error: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  clickSubmit = event => {
+    event.preventDefault();
+    const { name, email, password } = this.state;
+    const user = {
+      name,
+      email,
+      password
+    };
+    // console.log(user);
+    fetch("http://localhost:8080/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+      .then(response => {
+       console.log(JSON.stringify(response))
+        return response.json();
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    const { name, email, password } = this.state;
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">Signup</h2>
+
+        <form>
+          <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              className="form-control"
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input
+              onChange={this.handleChange}
+              type="email"
+              name="email"
+              className="form-control"
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input
+              onChange={this.handleChange}
+              type="password"
+              name="password"
+              className="form-control"
+              value={password}
+            />
+          </div>
+          <button
+            onClick={this.clickSubmit}
+            className="btn btn-raised btn-primary"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Signup;
