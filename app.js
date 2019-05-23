764c61670f09a6a09d178c4af9e162d929a1e1f0
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-23 16:19:35 +0545

    heroku deployment configuration

--- app.js
+++ app.js
@@ -53,6 +53,14 @@ app.use(function(err, req, res, next) {
   res.status(500).send("Something failed...");
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res)=>{
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
