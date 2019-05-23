baecd93765f53cc6a2201928ead6cbbb2372b8f6
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-23 17:58:21 +0545

    change base_url to the heroku in production

--- client/src/utils/Constants.js
+++ client/src/utils/Constants.js
@@ -1,5 +1,13 @@
-const appconstants = {
- base_url: process.env.PORT+'/api' || 'http://localhost:8080/api'
+var appconstants = {};
+
+if (process.env.NODE_ENV === "production") {
+  appconstants = {
+    base_url: "https://nepsanjaal.herokuapp.com/api"
+  };
+} else {
+  appconstants = {
+    base_url: "http://localhost:8080/api"
+  };
 }
 
 export default appconstants;
